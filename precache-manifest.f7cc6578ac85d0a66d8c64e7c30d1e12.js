f45d3eed012276dae934f0c41a93a9198598ef48
rtheni <rntm72@gmail.com>
2020-10-04 21:19:26 -0300

    Updates

---
+++ precache-manifest.f7cc6578ac85d0a66d8c64e7c30d1e12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc35842a0d8ef9e41d714c5ac7876d26",
+    "url": "/ProyectoModulo3Matea-Frontend/index.html"
+  },
+  {
+    "revision": "431f7f7e1b20529735be",
+    "url": "/ProyectoModulo3Matea-Frontend/static/css/main.02a02e5c.chunk.css"
+  },
+  {
+    "revision": "f3e85b24c7c8fe8b0600",
+    "url": "/ProyectoModulo3Matea-Frontend/static/js/2.386b3fda.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ProyectoModulo3Matea-Frontend/static/js/2.386b3fda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "431f7f7e1b20529735be",
+    "url": "/ProyectoModulo3Matea-Frontend/static/js/main.5e82ac0f.chunk.js"
+  },
+  {
+    "revision": "7ab7e2ab0faab7d133ac",
+    "url": "/ProyectoModulo3Matea-Frontend/static/js/runtime-main.c58ba68f.js"
+  }
+]);
